32b1c723845865b82f32860049f86adc9c92e253
Fernando Sproviero <fernandospr@gmail.com>
2018-07-08 15:42:52 -0300

    Adding logs

--- app.js
+++ app.js
@@ -11,15 +11,17 @@ app.post('/files', function(req, res) {
 
   if (!req.files)
     return res.status(400).send({ message: "multipart/form-data with 'file' field required to upload." });
- 
+
   let sampleFile = req.files.file;
+  console.log('Received file ' + sampleFile.name);
 
   let targetPath = __dirname + '/files/' + sampleFile.name;
  
   sampleFile.mv(targetPath, function(err) {
     if (err)
       return res.status(500).send({ message: err });
- 
+
+    console.log('File ' + sampleFile.name + ' copied to ' + targetPath);
     res.status(200).send({ url: "/files/" + sampleFile.name });
   });
 });
